95fee35bf595e5c45df23d475f84a226d43f30f0
Mateus <heloimateus@gmail.com>
2025-11-03 10:14:35 -0300

    Falha na pagina login

--- src/Services/Service.ts
+++ src/Services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoalbackendjs09.onrender.com/swagger'
+    baseURL: 'https://blogpessoalbackendjs09.onrender.com'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
